b76104f6549799f921bfa1da69ff32d50a0264e3
Abdulbaki Dursun <eggsydev@outlook.com>
2020-09-08 01:28:43 +0300

    url song fix

--- src/pages/redirect/_url.vue
+++ src/pages/redirect/_url.vue
@@ -113,9 +113,16 @@ export default {
         .doc(moment().format("DD.MM.YYYY"));
 
       const data = (await ref.get()).data();
-      const url = data?.url || "dQw4w9WgXcQ";
+	  
+	  const data = (await ref.get()).data();
+	  let songUrl = data?.url || "dQw4w9WgXcQ";
+	
+	  if (songUrl.includes("youtube.com/watch"))
+		songUrl = new URLSearchParams(new URL(data.url).search).get("v");
+      else if (songUrl.includes("youtu.be/"))
+		songUrl = songUrl.split("/")[songUrl.split("/").length - 1];
 
-      return redirect(`https://youtube.com/watch?v=${url}`);
+       return redirect(`https://youtube.com/watch?v=${songUrl}`);
     }
   },
   data() {
