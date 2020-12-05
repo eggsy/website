c56847bf116229d77b93a7a5d6b9aa084ce4c5f6
EGGSY <eggsydev@outlook.com>
2020-12-05 19:58:18 +0300

    🙏 extending routes?

--- config/router.js
+++ config/router.js
@@ -1,3 +1,10 @@
 export default {
   middleware: ["redirect"],
+  extendRoutes(routes, resolve) {
+    routes.push({
+      name: "discord",
+      path: "/discord/",
+      component: resolve(__dirname, "pages/index.vue"),
+    })
+  },
 }
