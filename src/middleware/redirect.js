feffe723122fc1b6c4e4354d1fe440475bad955c
EGGSY <eggsydev@outlook.com>
2020-12-05 19:51:12 +0300

    🙏 pls work

--- src/middleware/redirect.js
+++ src/middleware/redirect.js
@@ -1,7 +1,10 @@
 export default async function (ctx) {
   const dailySongPaths = ["/redirect/dailysong", "/api/dailysong", "/dailysong"]
+  const isDailyPath = () =>
+    dailySongPaths.includes(ctx.route.path.toLowerCase()) ||
+    dailySongPaths.includes(ctx.route.path.toLowerCase() + "/")
 
-  if (dailySongPaths.includes(ctx.route.path.toLowerCase()))
+  if (isDailyPath())
     ctx.redirect(`https://youtube.com/watch?v=${await ctx.$getDaily()}`)
   else if (ctx.route.path === "/discord")
     return ctx.redirect("https://discord.gg/tHx9ZKG")
