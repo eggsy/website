d1d8cdbe58123aadc1897eaf72bbf5961da51957
EGGSY <eggsydev@outlook.com>
2021-02-08 21:22:54 +0300

    ✨ create this component for daily song cards

---
+++ src/components/Card/Song.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="flex items-center px-4 py-2 space-x-2 bg-gray-100 rounded-md cursor-pointer select-none hover:bg-gray-200 dark:hover:bg-gray-700 ring-1 ring-gray-200 dark:ring-gray-800 dark:bg-gray-800"
+  >
+    <SkeletonLoader
+      type="image"
+      :image-url="thumbnail"
+      class="w-16 h-16 rounded-md bg-auto"
+    />
+
+    <div class="truncate">
+      <div class="flex items-center space-x-1">
+        <icon
+          v-if="getDateText.startsWith('Today')"
+          name="star"
+          class="w-4 h-4 text-gray-900 dark:text-gray-100"
+        />
+
+        <span class="text-sm text-gray-600 dark:text-gray-300">{{
+          getDateText
+        }}</span>
+      </div>
+
+      <h3
+        class="text-lg font-semibold leading-tight text-gray-900 truncate dark:text-gray-100"
+      >
+        {{ title }}
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    date: {
+      type: [String, Date],
+      required: true,
+      default: null,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+      default: null,
+    },
+  },
+  computed: {
+    /**
+     * Compares the dates between the provided date and current date and returns a title which will be used in cards' title.
+     * @returns {string} The title "Today's Song" or formatted date.
+     */
+    getDateText() {
+      if (
+        this.$moment(this.date).format("DD/MM/YYYY") ===
+        this.$moment().format("DD/MM/YYYY")
+      )
+        return "Today's Song"
+      else return this.$moment(this.date).format("DD/MM/YYYY")
+    },
+  },
+}
+</script>
