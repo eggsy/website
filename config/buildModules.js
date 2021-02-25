bb93646506a85f403197977e229003ef789d973b
EGGSY <eggsydev@outlook.com>
2021-02-26 01:55:23 +0300

    feat(nuxt-vite): implement nuxt-vite, WIP

--- config/buildModules.js
+++ config/buildModules.js
@@ -3,9 +3,12 @@ import colorMode from "./modules/colorMode"
 import tailwindcss from "./modules/tailwindcss"
 
 export default [
+  /*
+    Will keep this here until it get's updated an is ready to be used in production
+    IT'S FAST!
+  "nuxt-vite", */
   "@nuxtjs/svg",
   "@nuxtjs/moment",
-  "@nuxtjs/eslint-module",
   ["@nuxtjs/dotenv", dotenv],
   ["@nuxtjs/color-mode", colorMode],
   ["@nuxtjs/tailwindcss", tailwindcss],
