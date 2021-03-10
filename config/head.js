c12cc52617bec4ada2d9a522f854e035c5e1ee4b
EGGSY <eggsydev@outlook.com>
2021-03-10 21:12:56 +0300

    chore: remove unnecessary line

--- config/head.js
+++ config/head.js
@@ -3,9 +3,6 @@ const description =
 
 export default {
   title: "eggsy.xyz",
-  htmlAttrs: {
-    class: "scrollbar-thin",
-  },
   meta: [
     { charset: "utf-8" },
     { name: "viewport", content: "width=device-width, initial-scale=1" },
