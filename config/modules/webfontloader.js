1e2a8e21b1fc394bacae0bc03508f46efc63f498
EGGSY <eggsydev@outlook.com>
2021-03-19 03:01:38 +0300

    chore: remove unnecessary font import

--- config/modules/webfontloader.js
+++ config/modules/webfontloader.js
@@ -1,9 +1,8 @@
 export default {
   custom: {
-    families: ["Inter", "Quicksand"],
+    families: ["Inter"],
     urls: [
       "https://fonts.googleapis.com/css?family=Inter:wght@400;500;600;700&display=swap",
-      "https://fonts.googleapis.com/css?family=Quicksand:wght@400;500;600;700&display=swap",
     ],
   },
 }
