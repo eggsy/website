0e4400d5dbe78d2deda4abd7489397e04a0f8a22
EGGSY <eggsydev@outlook.com>
2021-03-19 03:02:29 +0300

    chore: order classes

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="min-h-screen min-w-screen bg-gray-50 dark:bg-gray-900">
     <div
-      class="bg-indigo-700 w-full py-2 top-0 right-0 left-0 absolute dark:bg-indigo-900"
+      class="absolute top-0 left-0 right-0 w-full py-2 bg-indigo-700 dark:bg-indigo-900"
     />
 
     <NavbarDefault class="pt-8" />
-    <Nuxt class="container mx-auto pb-10 w-11/12 sm:w-9/12 md:w-7/12" />
+    <Nuxt class="container w-11/12 pb-10 mx-auto sm:w-9/12 md:w-7/12" />
 
     <div
-      class="flex space-x-2 mr-6 mb-4 right-0 bottom-0 z-50 fixed items-center sm:space-x-4"
+      class="fixed bottom-0 right-0 z-50 flex items-center mb-4 mr-6 space-x-2 sm:space-x-4"
     >
       <GoTop />
     </div>
