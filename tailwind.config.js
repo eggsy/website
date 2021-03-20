2bde65267c22f6d65509b97fdb30542c4fb9e2d2
EGGSY <eggsydev@outlook.com>
2021-03-21 01:29:03 +0300

    chore: remove variants and theme extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,33 +6,8 @@ module.exports = {
     content: [resolve("./src/content/**/*")],
   },
   theme: {
-    extend: {
-      zIndex: {
-        "-1": "-1",
-        "-2": "-2",
-        "-3": "-3",
-        "-4": "-4",
-        "-5": "-5",
-      },
-      colors: {
-        social: {
-          twitter: "#1DA1F2",
-          telegram: "#2EAADE",
-          whatsapp: "#25D366",
-          patreon: "#FF424D",
-        },
-        brand: {
-          discord: { light: "#7289DA", dark: "#6c82cf" },
-        },
-      },
-    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
-    },
-    variants: {
-      extend: {
-        backgroundOpacity: ["dark"],
-      },
     },
   },
   plugins: [require("tailwind-scrollbar"), require("@tailwindcss/line-clamp")],
