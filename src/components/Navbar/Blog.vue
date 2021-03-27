b934abd7add4fa1438f8fa5b33f6737549bb485d
EGGSY <eggsydev@outlook.com>
2021-03-27 23:53:26 +0300

    chore: improve blog navbar

--- src/components/Navbar/Blog.vue
+++ src/components/Navbar/Blog.vue
@@ -2,13 +2,16 @@
   <nav
     :class="{
       'z-40 w-full': true,
-      'bg-indigo-700 dark:bg-indigo-900': searchEnabled === true,
+      'pt-8': searchEnabled === false,
+      'bg-indigo-700 dark:bg-indigo-900 pt-6': searchEnabled === true,
     }"
   >
-    <div class="container mx-auto text-gray-200 sm:px-0 sm:w-7/12">
+    <div
+      class="container w-11/12 mx-auto text-gray-200 sm:px-0 sm:w-9/12 md:w-7/12"
+    >
       <div
         v-if="searchEnabled === false"
-        class="flex items-center justify-between px-4 py-2 sm:px-0"
+        class="flex items-center justify-between"
       >
         <nuxt-link :to="{ name: getTargetRoute.name }">
           <SkeletonLoader
@@ -38,32 +41,36 @@
         </div>
       </div>
 
-      <div v-else-if="searchEnabled === true">
-        <div class="relative">
-          <label for="search" class="hidden">Ara</label>
-          <input
-            id="search"
-            ref="search"
-            v-model="input"
-            placeholder="Ne aramak istiyorsunuz?"
-            class="w-full py-4 pl-4 pr-24 placeholder-gray-100 bg-indigo-700 rounded-none sm:pr-28 dark:bg-indigo-900 focus:outline-none"
-            @keydown="handleSearchKeydown"
-          />
+      <div v-if="searchEnabled === true">
+        <div class="items-center justify-between md:space-x-4 md:flex">
+          <div class="w-full md:w-10/12">
+            <label for="search" class="hidden">Ara</label>
+            <input
+              id="search"
+              ref="search"
+              v-model="input"
+              placeholder="Ne aramak istiyorsunuz?"
+              class="w-full py-2 placeholder-gray-100 bg-indigo-700 rounded-none dark:bg-indigo-900 focus:outline-none"
+              @keydown="handleSearchKeydown"
+            />
+          </div>
+
           <div
-            class="absolute flex items-center space-x-1 text-xs select-none right-4 absolute-align-middle sm:space-x-2 sm:right-1 focus:outline-none"
+            class="flex items-center py-2 space-x-1 text-xs select-none md:w-2/12 sm:space-x-2 focus:outline-none"
           >
             <div
-              class="p-2 bg-indigo-600 rounded-md cursor-pointer hover:bg-indigo-800"
+              class="flex items-center justify-center w-1/2 p-2 space-x-2 bg-indigo-600 rounded-md cursor-pointer hover:bg-indigo-800"
               @click="search()"
             >
-              <span class="hidden sm:block">ENTER</span>
+              <span>ENTER</span>
               <icon name="search" class="block w-4 h-4 sm:hidden" />
             </div>
+
             <div
-              class="p-2 bg-indigo-600 rounded-md cursor-pointer hover:bg-indigo-800"
+              class="flex items-center justify-center w-1/2 p-2 space-x-2 bg-indigo-600 rounded-md cursor-pointer hover:bg-indigo-800"
               @click="searchEnabled = false"
             >
-              <span class="hidden sm:block">ESC</span>
+              <span>ESC</span>
               <icon name="x" class="block w-4 h-4 sm:hidden" />
             </div>
           </div>
@@ -79,7 +86,6 @@ export default {
     return {
       input: "",
       searchEnabled: false,
-      links: ["Haber", "Discord", "Linux"],
     }
   },
   computed: {
