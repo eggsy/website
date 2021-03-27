2e3a047a95eea28ff4918dbf0eaff245584eab71
EGGSY <eggsydev@outlook.com>
2021-03-27 23:00:52 +0300

    chore: improve repository skeleton

--- src/components/SkeletonLoader.vue
+++ src/components/SkeletonLoader.vue
@@ -1,15 +1,21 @@
 <template>
   <div
     v-if="type === 'repository'"
-    class="p-4 space-y-1 bg-gray-100 rounded-md hover:bg-gray-200 dark:hover:bg-opacity-80 dark:bg-gray-800 dark:hover:bg-gray-800"
+    class="p-4 space-y-1 bg-gray-100 rounded-md ring-1 ring-gray-200 dark:ring-gray-900 dark:bg-gray-800"
   >
-    <div
-      class="w-7/12 h-4 bg-gray-300 rounded-md dark:bg-gray-700 animate-pulse"
-    ></div>
+    <div class="flex items-center justify-between">
+      <div
+        class="w-7/12 h-5 bg-gray-300 rounded-md dark:bg-gray-700 animate-pulse"
+      />
+
+      <div
+        class="w-2/12 h-5 bg-gray-300 rounded-md dark:bg-gray-700 animate-pulse"
+      />
+    </div>
+
     <div class="space-y-px animate-pulse">
-      <div class="w-full h-3 bg-gray-300 rounded-md dark:bg-gray-700"></div>
-      <div class="w-11/12 h-3 bg-gray-300 rounded-md dark:bg-gray-700"></div>
-      <div class="w-4/12 h-3 bg-gray-300 rounded-md dark:bg-gray-700"></div>
+      <div class="w-full h-4 bg-gray-300 rounded-md dark:bg-gray-700" />
+      <div class="w-4/12 h-4 bg-gray-300 rounded-md dark:bg-gray-700" />
     </div>
   </div>
 
