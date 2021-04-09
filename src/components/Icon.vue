0f650294a3d55dff0b44141f1086ace1e8f73b66
EGGSY <eggsydev@outlook.com>
2021-04-09 23:34:26 +0300

    chore: use self-closing tags

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,21 +1,21 @@
 <template>
   <!-- eslint-disable vue/no-v-html -->
-  <div v-if="!name"></div>
-  <div v-else-if="!icon"></div>
+  <div v-if="!name" />
+  <div v-else-if="!icon" />
 
   <div
     v-else-if="typeof classes === 'string'"
     :class="`svg-icon ${classes}`"
     v-html="icon"
-  ></div>
+  />
 
   <div
     v-else-if="typeof classes === 'object'"
     :class="`svg-icon ${classes}`"
     v-html="icon"
-  ></div>
+  />
 
-  <div v-else class="svg-icon" v-html="icon"></div>
+  <div v-else class="svg-icon" v-html="icon" />
 </template>
 
 <script>
