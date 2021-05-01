973d26eebf333df9c7dd6aa88c16414773905622
EGGSY <eggsydev@outlook.com>
2021-05-01 19:30:42 +0300

    refactor: improve and add new images

--- src/assets/files/premid/smallImages.js
+++ src/assets/files/premid/smallImages.js
@@ -1,27 +1,49 @@
-export default {
-  DoNotDisturb: "https://i.imgur.com/MvrnrMW.png",
-  Brilliance: "https://i.imgur.com/skclu5h.png",
-  VideoCall: "https://i.imgur.com/GwMdYmz.png",
-  Checkmark: "https://i.imgur.com/FH7OH6y.png",
-  Thinking: "https://i.imgur.com/mzlKBw3.png",
-  Windows: "https://i.imgur.com/YkOU4HD.png",
-  NoEntry: "https://i.imgur.com/jVidfcx.png",
-  Balance: "https://i.imgur.com/v0jG4vt.png",
-  Bravery: "https://i.imgur.com/etvIz6E.png",
-  Playing: "https://i.imgur.com/UHgwoyQ.png",
-  Writing: "https://i.imgur.com/4VcqgYA.png",
-  Reading: "https://i.imgur.com/tbSYfJV.png",
-  Coffee: "https://i.imgur.com/W5NIvJF.png",
-  Online: "https://i.imgur.com/8cel80u.png",
-  Please: "https://i.imgur.com/d10KCzD.png",
-  Paused: "https://i.imgur.com/TYvgF3M.png",
-  Search: "https://i.imgur.com/hnDIQO1.png",
-  Mmlol: "https://i.imgur.com/5t2q2eu.png",
-  Heart: "https://i.imgur.com/jtt9fjf.png",
-  Linux: "https://i.imgur.com/bN5rmiU.png",
-  Live: "https://i.imgur.com/qphbAuR.png",
-  Call: "https://i.imgur.com/0akjqyz.png",
-  Idle: "https://i.imgur.com/mKIQ8Zo.png",
-  Cool: "https://i.imgur.com/AdUBBHa.png",
-  Tada: "https://i.imgur.com/nO8fd9v.png",
-}
+const general = [
+  { name: "DoNotDisturb", url: "https://i.imgur.com/MvrnrMW.png" },
+  { name: "Brilliance", url: "https://i.imgur.com/skclu5h.png" },
+  { name: "VideoCall", url: "https://i.imgur.com/GwMdYmz.png" },
+  { name: "Checkmark", url: "https://i.imgur.com/FH7OH6y.png" },
+  { name: "Thinking", url: "https://i.imgur.com/mzlKBw3.png" },
+  { name: "Windows", url: "https://i.imgur.com/YkOU4HD.png" },
+  { name: "NoEntry", url: "https://i.imgur.com/jVidfcx.png" },
+  { name: "Balance", url: "https://i.imgur.com/v0jG4vt.png" },
+  { name: "Bravery", url: "https://i.imgur.com/etvIz6E.png" },
+  { name: "Playing", url: "https://i.imgur.com/UHgwoyQ.png" },
+  { name: "Writing", url: "https://i.imgur.com/4VcqgYA.png" },
+  { name: "Reading", url: "https://i.imgur.com/tbSYfJV.png" },
+  { name: "Coffee", url: "https://i.imgur.com/W5NIvJF.png" },
+  { name: "Online", url: "https://i.imgur.com/8cel80u.png" },
+  { name: "Please", url: "https://i.imgur.com/d10KCzD.png" },
+  { name: "Paused", url: "https://i.imgur.com/TYvgF3M.png" },
+  { name: "Search", url: "https://i.imgur.com/hnDIQO1.png" },
+  { name: "Mmlol", url: "https://i.imgur.com/5t2q2eu.png" },
+  { name: "Heart", url: "https://i.imgur.com/jtt9fjf.png" },
+  { name: "Linux", url: "https://i.imgur.com/bN5rmiU.png" },
+  { name: "Live", url: "https://i.imgur.com/qphbAuR.png" },
+  { name: "Call", url: "https://i.imgur.com/0akjqyz.png" },
+  { name: "Idle", url: "https://i.imgur.com/mKIQ8Zo.png" },
+  { name: "Cool", url: "https://i.imgur.com/AdUBBHa.png" },
+  { name: "Tada", url: "https://i.imgur.com/nO8fd9v.png" },
+]
+
+const brand = [
+  { name: "YouTube", url: "https://i.imgur.com/0Bvl6BU.png" },
+  { name: "Netflix", url: "https://i.imgur.com/DkZQvkC.png" },
+  { name: "Twitter", url: "https://i.imgur.com/AtV70mE.png" },
+  { name: "Twitch", url: "https://i.imgur.com/bmIsItf.png" },
+  { name: "Discord", url: "https://i.imgur.com/P6fs8jR.png" },
+]
+
+/* Exports */
+export default [
+  {
+    name: "General",
+    items: general,
+  },
+  {
+    name: "Brand",
+    items: brand,
+  },
+]
+
+export const smallAll = [...general, ...brand]
