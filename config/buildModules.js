930709d6bdcba6d0c53574d7a9b009130870299c
EGGSY <eggsydev@outlook.com>
2021-05-05 16:03:10 +0300

    chore: remove nuxt-storm who needs webstorm anyway

--- config/buildModules.js
+++ config/buildModules.js
@@ -1,6 +1,5 @@
 import dotenv from "./modules/dotenv"
 import colorMode from "./modules/colorMode"
-import nuxtStorm from "./modules/nuxtStorm"
 import tailwindcss from "./modules/tailwindcss"
 import googleAnalytics from "./modules/googleAnalytics"
 
@@ -8,7 +7,6 @@ export default [
   "@nuxtjs/svg",
   "@nuxt/image",
   "@nuxtjs/moment",
-  ["nuxt-storm", nuxtStorm],
   ["@nuxtjs/dotenv", dotenv],
   ["@nuxtjs/color-mode", colorMode],
   ["@nuxtjs/tailwindcss", tailwindcss],
--- config/modules/nuxtStorm.js
+++
@@ -1,3 +0,0 @@
-export default {
-  nested: true,
-}
