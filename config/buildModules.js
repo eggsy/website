a1e057a7e75396882435e5128c982dce25db319b
EGGSY <eggsydev@outlook.com>
2021-05-06 03:12:49 +0300

    feat: add windicss

--- config/buildModules.js
+++ config/buildModules.js
@@ -1,14 +1,13 @@
 import dotenv from "./modules/dotenv"
 import colorMode from "./modules/colorMode"
-import tailwindcss from "./modules/tailwindcss"
 import googleAnalytics from "./modules/googleAnalytics"
 
 export default [
   "@nuxtjs/svg",
   "@nuxt/image",
+  "nuxt-windicss",
   "@nuxtjs/moment",
   ["@nuxtjs/dotenv", dotenv],
   ["@nuxtjs/color-mode", colorMode],
-  ["@nuxtjs/tailwindcss", tailwindcss],
   ["@nuxtjs/google-analytics", googleAnalytics],
 ]
