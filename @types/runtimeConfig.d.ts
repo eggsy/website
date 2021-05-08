60f510f5076a1d785763d99217f126d14008a465
EGGSY <eggsydev@outlook.com>
2021-05-08 17:22:50 +0300

    feat: extend nuxtruntimeconfig for intellisense

---
+++ @types/runtimeConfig.d.ts
@@ -0,0 +1,35 @@
+declare module "@nuxt/types/config/runtime" {
+  interface NuxtRuntimeConfig {
+    social: Social
+    sponsor: Sponsor
+    links: Links
+  }
+}
+
+/* Interfaces */
+export interface Sponsor {
+  patreon: string
+}
+
+export interface Social {
+  discord: string
+  twitter: string
+  github: string
+  instagram: string
+  trello: string
+}
+
+export interface Links {
+  unblockPlease: UnblockPlease
+  isInsideMe: IsInsideMe
+}
+
+export interface IsInsideMe {
+  website: string
+  faq: string
+}
+
+export interface UnblockPlease {
+  webstore: string
+  github: string
+}
