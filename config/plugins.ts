9455f3b3e8888ba9a81222abbada987dad951d3d
EGGSY <eggsydev@outlook.com>
2021-05-08 06:19:11 +0300

    feat: add disqus globally

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,6 +2,7 @@ import { NuxtOptionsPlugin } from "@nuxt/types/config/plugin"
 
 const Plugins: NuxtOptionsPlugin[] = [
   "@/plugins/Util",
+  "@/plugins/Disqus",
   {
     src: "@/plugins/Firebase",
     mode: "client",
---
+++ src/plugins/Disqus.ts
@@ -0,0 +1,6 @@
+import Vue from "vue"
+import VueDisqus from "vue-disqus"
+
+Vue.use(VueDisqus, {
+  shortname: "eggsy-xyz",
+})
