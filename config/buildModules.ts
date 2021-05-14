ecf00c8417fc64b3f66666e053b889e1a2b481ee
EGGSY <eggsydev@outlook.com>
2021-05-14 03:41:54 +0300

    feat: use nuxt-vite

--- config/buildModules.ts
+++ config/buildModules.ts
@@ -1,17 +1,16 @@
 import { NuxtOptionsModule } from "@nuxt/types/config/module"
 
 /* Import module options */
-import dotenv from "./modules/dotenv"
 import colorMode from "./modules/colorMode"
 import googleAnalytics from "./modules/googleAnalytics"
 import typescriptBuild from "./modules/typescriptBuild"
 
 const BuildModules: NuxtOptionsModule[] = [
-  "@nuxtjs/svg",
+  "nuxt-vite",
+  /* "@nuxtjs/svg", */
   "@nuxt/image",
   "nuxt-windicss",
   "@nuxtjs/moment",
-  ["@nuxtjs/dotenv", dotenv],
   ["@nuxtjs/color-mode", colorMode],
   ["@nuxt/typescript-build", typescriptBuild],
   ["@nuxtjs/google-analytics", googleAnalytics],
