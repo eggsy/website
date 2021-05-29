1045123108af5939beda88f3caf9a800072b6c79
EGGSY <eggsydev@outlook.com>
2021-05-29 16:01:15 +0300

    refactor: use windicss module options

--- config/buildModules.ts
+++ config/buildModules.ts
@@ -2,6 +2,7 @@ import { NuxtOptionsModule } from "@nuxt/types/config/module"
 
 /* Import module options */
 import colorMode from "./modules/colorMode"
+import windicss from "./modules/windicss"
 import googleAnalytics from "./modules/googleAnalytics"
 import typescriptBuild from "./modules/typescriptBuild"
 
@@ -13,8 +14,8 @@ const BuildModules: NuxtOptionsModule[] = [
   /* "nuxt-swc", */
   "nuxt-vite",
   "@nuxt/image",
-  "nuxt-windicss",
   "@nuxtjs/moment",
+  ["nuxt-windicss", windicss],
   ["@nuxtjs/color-mode", colorMode],
   ["@nuxt/typescript-build", typescriptBuild],
   ["@nuxtjs/google-analytics", googleAnalytics],
---
+++ config/modules/windicss.ts
@@ -0,0 +1,8 @@
+import { resolve } from "path"
+import { UserOptions } from "@windicss/plugin-utils"
+
+const windicss: UserOptions = {
+  config: resolve("windi.config.ts"),
+}
+
+export default windicss
