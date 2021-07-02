1d1bc347f3fe1a23a02e3ff73fda071184ae65d2
Abdulbaki Dursun <eggsydev@gmail.com>
2021-07-02 22:07:49 +0300

    refactor: add nuxt image options

--- config/buildModules.ts
+++ config/buildModules.ts
@@ -3,13 +3,14 @@ import { NuxtOptionsModule } from "@nuxt/types/config/module"
 /* Import module options */
 import colorMode from "./modules/colorMode"
 import windicss from "./modules/windicss"
+import image from "./modules/image"
 import googleAnalytics from "./modules/googleAnalytics"
 import typescriptBuild from "./modules/typescriptBuild"
 
 const BuildModules: NuxtOptionsModule[] = [
   "nuxt-vite",
-  "@nuxt/image",
   "@nuxtjs/moment",
+  ["@nuxt/image", image],
   ["nuxt-windicss", windicss],
   ["@nuxtjs/color-mode", colorMode],
   ["@nuxt/typescript-build", typescriptBuild],
---
+++ config/modules/image.ts
@@ -0,0 +1,5 @@
+const NuxtImage = {
+  domains: ["https://i.vgy.me", "https://i.imgur.com"],
+}
+
+export default NuxtImage
