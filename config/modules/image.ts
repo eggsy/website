1bf187b2bf745f1f77b51bc3016b3f49abd137a6
Abdulbaki Dursun <eggsydev@gmail.com>
2021-07-05 00:07:02 +0300

    fix: add ytimg.com to allowed domains

--- config/modules/image.ts
+++ config/modules/image.ts
@@ -1,5 +1,5 @@
 const NuxtImage = {
-  domains: ["https://i.vgy.me", "https://i.imgur.com"],
+  domains: ["https://i.vgy.me", "https://i.imgur.com", "https://i.ytimg.com"],
 }
 
 export default NuxtImage
