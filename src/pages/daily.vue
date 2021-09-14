833dd3f0237355f7d814954bc715b33c6d3a5820
Abdulbaki Dursun <eggsydev@gmail.com>
2021-09-14 12:51:27 +0300

    refactor: remove lyrics tab as ksoft closed the api

--- src/pages/daily.vue
+++ src/pages/daily.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="space-y-6 py-4">
+  <div class="py-4 space-y-6">
     <BlogNotification v-if="isThereNoSongToday === true" type="warning">
       There's no song for today, check back later or wait for the next day until
       I find some time to add new songs! You can listen to the older ones if you
@@ -11,191 +11,67 @@
         <SkeletonLoader
           type="iframe"
           :iframe-url="
-            $fetchState.pending === false
-              ? `https://www.youtube.com/embed/${getSelectedSong.youtube}`
-              : ''
+            $fetchState.pending === false &&
+            `https://www.youtube.com/embed/${getSelectedSong.youtube}`
           "
-          class="h-56 w-full"
+          class="w-full h-full"
         />
-
-        <div class="space-y-2">
-          <div class="grid gap-2 items-center sm:grid-cols-2">
-            <SmartLink
-              :href="
-                $fetchState.pending === false
-                  ? `https://youtu.be/${getSelectedSong.youtube}/`
-                  : false
-              "
-              class="rounded cursor-pointer flex space-x-2 bg-gray-200 bg-opacity-40 text-center py-2 px-4 text-gray-900 items-center justify-center overflow-hidden select-none dark:text-gray-100"
-              :class="{
-                'hover:bg-gray-200 dark:(bg-gray-800 hover:bg-gray-700)':
-                  $fetchState.pending === false && $fetchState.error === null,
-              }"
-              blank
-            >
-              <SkeletonLoader
-                v-if="$fetchState.pending || $fetchState.error"
-                class="bg-gray-300 h-6 w-2/3 dark:bg-gray-700"
-              />
-
-              <template v-else>
-                <IconBrand brand="youtube" class="flex-shrink-0 h-6 w-6" />
-                <span class="truncate">YouTube</span>
-              </template>
-            </SmartLink>
-
-            <SmartLink
-              :href="
-                getSelectedSong.spotify
-                  ? `https://open.spotify.com/track/${getSelectedSong.spotify}/`
-                  : false
-              "
-              class="rounded cursor-pointer flex space-x-2 bg-gray-200 bg-opacity-40 py-2 px-4 text-gray-900 items-center justify-center overflow-hidden select-none dark:text-gray-100"
-              :class="{
-                'hover:bg-gray-200 dark:hover:bg-gray-700':
-                  $fetchState.pending === false && $fetchState.error === null,
-                'dark:bg-gray-800':
-                  $fetchState.pending === true || getSelectedSong.spotify,
-                'cursor-not-allowed bg-gray-200 bg-opacity-40 dark:bg-gray-700':
-                  $fetchState.pending === false &&
-                  $fetchState.error === null &&
-                  getSelectedSong.spotify === null,
-              }"
-              blank
-            >
-              <SkeletonLoader
-                v-if="$fetchState.pending || $fetchState.error"
-                class="bg-gray-300 h-6 w-2/3 dark:bg-gray-700"
-              />
-
-              <template v-else>
-                <IconBrand brand="spotify" class="flex-shrink-0 h-6 w-6" />
-                <span class="truncate">Spotify</span>
-              </template>
-            </SmartLink>
-          </div>
-
-          <div class="space-y-2">
-            <div
-              class="rounded bg-gray-200 bg-opacity-40 w-full grid py-2 px-4 grid-cols-4 items-center justify-between dark:bg-gray-800"
-            >
-              <span class="text-gray-900 dark:text-gray-100">Title</span>
-
-              <SkeletonLoader
-                v-if="$fetchState.pending || $fetchState.error"
-                class="bg-gray-300 h-4 col-start-3 col-end-5 dark:bg-gray-700"
-              />
-
-              <span
-                v-else
-                class="text-right text-gray-700 col-span-3 truncate dark:text-gray-300"
-              >
-                {{ getSelectedTitle }}
-              </span>
-            </div>
-
-            <div
-              class="rounded bg-gray-200 bg-opacity-40 w-full grid py-2 px-4 grid-cols-4 items-center justify-between dark:bg-gray-800"
-            >
-              <span class="text-gray-900 dark:text-gray-100">Artist</span>
-
-              <SkeletonLoader
-                v-if="$fetchState.pending || $fetchState.error"
-                class="bg-gray-300 h-4 col-start-4 col-end-5 dark:bg-gray-700"
-              />
-
-              <span
-                v-else
-                class="text-right text-gray-700 col-span-3 truncate dark:text-gray-300"
-              >
-                {{ getSelectedSongMetadata.artist || "Unknown" }}
-              </span>
-            </div>
-
-            <div
-              class="rounded bg-gray-200 bg-opacity-40 w-full grid py-2 px-4 grid-cols-4 items-center justify-between dark:bg-gray-800"
-            >
-              <span class="text-gray-900 dark:text-gray-100">Date</span>
-
-              <SkeletonLoader
-                v-if="$fetchState.pending || $fetchState.error"
-                class="bg-gray-300 h-4 col-start-2 col-end-5 dark:bg-gray-700"
-              />
-
-              <span
-                v-else
-                class="text-right text-gray-700 col-span-3 truncate dark:text-gray-300"
-              >
-                {{ getSelectedDateTitle }}
-              </span>
-            </div>
-          </div>
-        </div>
       </div>
 
-      <div
-        class="rounded bg-gray-200 bg-opacity-40 h-52 p-2 relative sm:h-auto dark:bg-gray-800"
-        :class="{
-          'hidden sm:flex items-center justify-center': !getLyrics,
-        }"
-      >
-        <div
-          v-if="$fetchState.pending === true"
-          class="space-y-2 top-4 right-4 bottom-4 left-4 absolute overflow-y-auto scrollbar"
-        >
-          <div
-            v-for="lyric in 20"
-            :key="`lyric-skeleton-${lyric}`"
-            class="rounded bg-gray-200 h-4 animate-pulse dark:bg-gray-700"
-            :class="{
-              'w-3/4': lyric % 1 === 0,
-              'w-2/4': lyric % 2 === 0,
-              'w-3/5': lyric % 3 === 0,
-              'w-4/5': lyric % 4 === 0,
-            }"
+      <div class="space-y-2">
+        <div class="p-4 rounded-md bg-gray-200/50 dark:bg-gray-800">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100">
+            Title
+          </h3>
+
+          <SkeletonLoader
+            v-if="$fetchState.pending || $fetchState.error"
+            class="w-2/4 h-6 bg-gray-300 dark:bg-gray-700"
           />
+
+          <span v-else class="text-gray-700 truncate dark:text-gray-300">
+            {{ getSelectedTitle }}
+          </span>
         </div>
 
-        <div
-          v-else-if="typeof getLyrics === 'object' && getLyrics.length > 0"
-          class="space-y-2 top-4 right-4 bottom-4 left-4 absolute overflow-y-auto scrollbar"
-        >
-          <p
-            v-for="(lyric, index) in getLyrics"
-            :key="`lyric-${index}`"
-            :class="{
-              'text-gray-900 dark:text-gray-100': true,
-              'py-2': lyric === '' && index !== 0,
-              'font-semibold': index % 2 === 0,
-            }"
-          >
-            {{ lyric }}
-          </p>
+        <div class="p-4 rounded-md bg-gray-200/50 dark:bg-gray-800">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100">
+            Artist
+          </h3>
 
-          <p class="font-medium text-xs text-gray-600 dark:text-gray-400">
-            Lyrics by
-            <SmartLink
-              href="https://api.ksoft.si/"
-              class="hover:underline"
-              blank
-            >
-              KSoft.Si
-            </SmartLink>
-          </p>
+          <SkeletonLoader
+            v-if="$fetchState.pending || $fetchState.error"
+            class="w-1/4 h-6 bg-gray-300 dark:bg-gray-700"
+          />
+
+          <span v-else class="text-gray-700 truncate dark:text-gray-300">
+            {{ getSelectedSongMetadata.artist || "Unknown" }}
+          </span>
         </div>
 
-        <div v-else class="flex h-full items-center justify-center">
-          <h3 class="text-lg text-gray-700 dark:text-gray-300">No lyrics...</h3>
+        <div class="p-4 rounded-md bg-gray-200/50 dark:bg-gray-800">
+          <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100">
+            Date
+          </h3>
+
+          <SkeletonLoader
+            v-if="$fetchState.pending || $fetchState.error"
+            class="w-2/4 h-6 bg-gray-300 dark:bg-gray-700"
+          />
+
+          <span v-else class="text-gray-700 truncate dark:text-gray-300">
+            {{ getSelectedDateTitle }}
+          </span>
         </div>
       </div>
     </div>
 
     <div>
-      <h3 class="font-semibold text-xl text-gray-900 dark:text-gray-100">
+      <h3 class="text-xl font-semibold text-gray-900 dark:text-gray-100">
         Older Songs
       </h3>
 
-      <div class="mt-2 grid gap-2 sm:grid-cols-2 md:grid-cols-3">
+      <div class="grid gap-2 mt-2 sm:grid-cols-2 md:grid-cols-3">
         <template v-if="$fetchState.pending === true">
           <SkeletonLoader
             v-for="item in 9"
@@ -244,7 +120,6 @@ export default Vue.extend({
       metadata: {
         title: null,
         artist: null,
-        lyrics: null,
         thumbnail: null,
       },
     }
@@ -333,12 +208,6 @@ export default Vue.extend({
      */
     getSongList(): Song[] {
       return this.songs.filter((song: Song) => song.date !== this.selected.date)
-    },
-    /**
-     * Returns lyrics in array format if they were fetched from the API.
-     */
-    getLyrics(): string[] | boolean {
-      return this.selected?.metadata?.lyrics || false
     },
   },
 })
