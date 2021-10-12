57ba5af3ef956af344972f45e122ea83bd6e9e54
Abdulbaki Dursun <eggsydev@gmail.com>
2021-10-12 22:44:46 +0200

    feat: add image to rss feeds

--- config/modules/feed.ts
+++ config/modules/feed.ts
@@ -20,11 +20,15 @@ const Feed = () => {
 
     articles.forEach((article: any) => {
       const url = `${baseUrlArticles}/gonderi/${article.slug}`
+      const postImagesPath = `https://eggsy.xyz/assets/images/posts`
 
       feed.addItem({
         title: article.title,
         slug: article.slug,
         link: url,
+        image: article.image
+          ? `${postImagesPath}/${article.image}`
+          : `${postImagesPath}/${url.split("/").at(-1)}.jpg`,
         date: new Date(article.createdAt),
         description: article.description,
         content: article.summary,
