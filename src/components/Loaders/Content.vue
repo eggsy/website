d01bc2d47d2565b122910412d5fa5db584bb0013
Abdulbaki Dursun <eggsydev@gmail.com>
2021-11-20 00:57:18 +0100

    refactor: change position of items in loader

--- src/components/Loaders/Content.vue
+++ src/components/Loaders/Content.vue
@@ -16,29 +16,29 @@ export default Vue.extend({
   <!-- Loader -->
   <div v-if="error === false" class="w-full pt-14">
     <section class="space-y-4">
-      <!-- Title -->
-      <div class="w-full h-10 bg-gray-200 dark:bg-gray-700 anime-pulse sm:w-8/12" />
-
-      <!-- Content -->
-      <div class="flex flex-col items-center justify-center space-y-2 sm:items-start">
-        <div class="w-full h-4 bg-gray-200 dark:bg-gray-700 anime-pulse sm:w-8/12" />
-        <div class="w-1/2 h-4 bg-gray-200 dark:bg-gray-700 anime-pulse sm:w-4/12" />
+      <!-- Reading time & date  -->
+      <div class="flex space-x-2 justify-center sm:justify-start">
+        <div class="bg-gray-200 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-gray-700" />
+        <div class="bg-gray-200 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-gray-700" />
       </div>
 
-      <!-- Reading time & date  -->
-      <div class="flex justify-center space-x-2 sm:justify-start">
-        <div class="w-1/4 h-4 bg-gray-200 dark:bg-gray-700 animate-pulse sm:w-1/6" />
-        <div class="w-1/4 h-4 bg-gray-200 dark:bg-gray-700 animate-pulse sm:w-1/6" />
+      <!-- Title -->
+      <div class="bg-gray-200 h-10 w-full anime-pulse sm:w-8/12 dark:bg-gray-700" />
+
+      <!-- Content -->
+      <div class="flex flex-col space-y-2 items-center justify-center sm:items-start">
+        <div class="bg-gray-200 h-4 w-full anime-pulse sm:w-8/12 dark:bg-gray-700" />
+        <div class="bg-gray-200 h-4 w-1/2 anime-pulse sm:w-4/12 dark:bg-gray-700" />
       </div>
     </section>
 
     <!-- Content -->
-    <div class="w-full mt-10 space-y-10">
+    <div class="space-y-10 mt-10 w-full">
       <div class="space-y-1">
         <div
           v-for="_ in 6"
           :key="_"
-          class="h-4 bg-gray-200 dark:bg-gray-700 animate-pulse"
+          class="bg-gray-200 h-4 animate-pulse dark:bg-gray-700"
           :class="_ !== 6 ? 'w-full' : 'w-8/12'"
         />
       </div>
@@ -47,7 +47,7 @@ export default Vue.extend({
         <div
           v-for="_ in 4"
           :key="_"
-          class="h-4 bg-gray-200 dark:bg-gray-700 animate-pulse"
+          class="bg-gray-200 h-4 animate-pulse dark:bg-gray-700"
           :class="_ !== 4 ? 'w-full' : 'w-1/2'"
         />
       </div>
@@ -56,7 +56,7 @@ export default Vue.extend({
         <div
           v-for="_ in 24"
           :key="_"
-          class="h-4 bg-gray-200 dark:bg-gray-700 animate-pulse"
+          class="bg-gray-200 h-4 animate-pulse dark:bg-gray-700"
           :class="_ !== 24 ? 'w-full' : 'w-1/2'"
         />
       </div>
@@ -64,7 +64,7 @@ export default Vue.extend({
   </div>
 
   <div v-else class="space-y-4 pt-14">
-    <h1 class="text-2xl font-semibold text-gray-900 dark:text-gray-100">Gönderi yüklenemedi.</h1>
+    <h1 class="font-semibold text-2xl text-gray-900 dark:text-gray-100">Gönderi yüklenemedi.</h1>
     <p class="text-gray-700 dark:text-gray-300">
       Aradığınız gönderinin blogumda olduğuna, adreste bir yanlışlık olmadığına
       emin olun. Arama özelliğini kullanarak gönderiyi aratabilir, anasayfaya
