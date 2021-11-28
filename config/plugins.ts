5e76591912da25a773e9059503607ffd23f25e56
Abdulbaki Dursun <eggsydev@gmail.com>
2021-11-29 00:34:57 +0100

    refactor: add two new plugins

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,7 +2,9 @@ import { NuxtOptionsPlugin } from "@nuxt/types/config/plugin"
 
 const Plugins: NuxtOptionsPlugin[] = [
   "@/plugins/Util",
+  "@/plugins/Types",
   "@/plugins/Disqus",
+  "@/plugins/MomentTimezone",
   {
     src: "@/plugins/Lanyard",
     mode: "client",
