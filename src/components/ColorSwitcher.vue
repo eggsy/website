db212bba98f13063c02889a54ea27d3e115e3c1a
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-13 19:21:07 +0100

    refactor: use button component

--- src/components/ColorSwitcher.vue
+++ src/components/ColorSwitcher.vue
@@ -24,12 +24,8 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div
-    class="rounded-full cursor-pointer bg-gray-100 p-2 transition-all text-gray-900 select-none dark:(bg-neutral-800 text-gray-100) hover:(dark:bg-gray-100 dark:text-gray-900 light:bg-gray-800 text-gray-100 shadow-md) focus:outline-none "
-    @click="switchTheme"
-  >
+  <Button rounded elevated @click.native="switchTheme">
     <IconSun v-if="getSelectedTheme === 'light'" class="h-5 w-5" />
-
     <IconMoon v-else class="h-5 w-5" />
-  </div>
+  </Button>
 </template>
