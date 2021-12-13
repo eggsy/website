d97c6c5afd7cda569cc9d3af6cf2e84d5b97dfd3
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-13 20:39:04 +0100

    refactor: improve design

--- src/components/Card/Repository.vue
+++ src/components/Card/Repository.vue
@@ -49,7 +49,7 @@ export default Vue.extend({
 
 <template>
   <div
-    class="rounded-lg cursor-pointer h-full p-4 transition-colors text-gray-500 focusRing relative dark:text-neutral-500 hover:bg-gray-200/40 dark:hover:bg-neutral-800/40"
+    class="rounded-lg cursor-pointer flex flex-col h-full p-4 transition-colors text-gray-500 justify-between focusRing relative dark:text-neutral-500 hover:bg-gray-200/40 dark:hover:bg-neutral-800/40"
     :class="{
       'bg-gray-200/40 dark:bg-neutral-800/40': top,
     }"
