c91084f84e670e6951cd6cfdd1fc6d091330f861
Abdulbaki Dursun <eggsydev@gmail.com>
2021-12-13 18:49:22 +0100

    feat: improve experience component

--- src/components/Card/Experience.vue
+++ src/components/Card/Experience.vue
@@ -28,24 +28,19 @@ export default Vue.extend({
 </script>
 
 <template>
-  <div class="overflow-hidden">
-    <SmartLink
-      :href="url"
-      class="text-gray-800 dark:text-neutral-200 hover:underline"
-      blank
-    >
-      <h3 class="text-lg">{{ title }}</h3>
-    </SmartLink>
+  <div class="text-gray-500 overflow-hidden dark:text-neutral-700">
+    <div class="flex items-center justify-between">
+      <SmartLink
+        :href="url"
+        class="text-gray-800 dark:text-neutral-200 hover:underline"
+        blank
+      >
+        <h3 class="text-lg">{{ title }}</h3>
+      </SmartLink>
 
-    <div
-      class="flex space-x-2 divide-x-2 divide-gray-300 text-sm text-gray-500 items-center dark:(divide-neutral-700 text-neutral-500) "
-    >
-      <div class="flex space-x-2 flex-shrink-0 items-center">
-        <IconClock class="h-4 w-4" />
-        <span>{{ date }}</span>
-      </div>
-
-      <div v-if="position" class="pl-2 truncate">{{ position }}</div>
+      <span>{{ date }}</span>
     </div>
+
+    <div v-if="position" class="truncate">{{ position }}</div>
   </div>
 </template>
