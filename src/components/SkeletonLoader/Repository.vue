a1ef4871c265e4ac967e066033ebe149bd56da25
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-05 16:27:06 +0300

    refactor: improve skeleton

--- src/components/SkeletonLoader/Repository.vue
+++ src/components/SkeletonLoader/Repository.vue
@@ -1,12 +1,10 @@
 <template>
-  <div
-    class="rounded-md space-y-4 bg-gray-200 bg-opacity-40 p-4 dark:bg-neutral-800/40"
-  >
+  <div class="rounded-md space-y-4 p-4">
     <div
       class="rounded-md bg-gray-300 h-5 animate-pulse w-7/12 dark:bg-neutral-700/40"
     />
 
-    <div class="space-y-1">
+    <div class="space-y-2">
       <div
         class="rounded-md bg-gray-300 h-4 w-full animate-pulse dark:bg-neutral-700/40"
       />
@@ -16,6 +14,15 @@
     </div>
 
     <div class="space-y-2">
+      <div class="flex items-center justify-between">
+        <div
+          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+        />
+        <div
+          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+        />
+      </div>
+
       <div class="flex items-center justify-between">
         <div
           class="rounded-md bg-gray-300 h-4 animate-pulse w-2/6 dark:bg-neutral-700/40"
--- src/components/SkeletonLoader/Song.vue
+++ src/components/SkeletonLoader/Song.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="rounded-lg cursor-pointer flex space-x-2 bg-gray-200 bg-opacity-40 py-2 px-4 transition-shadow items-center select-none dark:bg-neutral-800 hover:shadow-md"
+    class="rounded-lg cursor-pointer flex space-x-2 p-2 transition-shadow items-center select-none hover:shadow-md"
   >
     <div
       class="rounded-md bg-gray-200 flex-shrink-0 h-16 animate-pulse w-16 dark:bg-neutral-700/40"
@@ -8,11 +8,11 @@
 
     <div class="flex-grow space-y-1">
       <div
-        class="rounded bg-gray-200 h-4 animate-pulse w-1/3 dark:bg-neutral-700/40"
+        class="rounded bg-gray-200 h-4 animate-pulse w-2/3 dark:bg-neutral-700/40"
       />
 
       <div
-        class="rounded bg-gray-200 h-4 animate-pulse w-2/3 dark:bg-neutral-700/40"
+        class="rounded bg-gray-200 h-4 animate-pulse w-1/3 dark:bg-neutral-700/40"
       />
     </div>
   </div>
