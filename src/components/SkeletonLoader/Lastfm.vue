5aee8805a5ed170eea820aee3cfd883a81777fbb
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-06 05:20:18 +0300

    feat: lastfm card skeleton loader

---
+++ src/components/SkeletonLoader/Lastfm.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  computed: {
+    getRandomItem(): string {
+      return ["w-2/3", "w-3/4", "w-2/5"][Math.floor(Math.random() * 3)]
+    },
+  },
+})
+</script>
+
+<template>
+  <div
+    class="rounded-md flex space-x-4 p-4 transition-colors focusRing overflow-x-hidden items-center"
+    :class="{
+      'justify-between': plays !== false,
+    }"
+  >
+    <div class="flex space-x-4 w-full items-center">
+      <SkeletonLoader class="rounded-md flex-shrink-0 h-16 w-16" />
+
+      <div class="flex flex-col space-y-1 w-full">
+        <SkeletonLoader class="h-5" :class="getRandomItem" />
+        <SkeletonLoader class="h-5 w-1/3" />
+      </div>
+    </div>
+  </div>
+</template>
