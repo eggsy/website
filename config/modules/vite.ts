44782d4804d6d4ad8d772aca83b45ba428fef038
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-10 11:46:59 +0100

    feat: exclude modules from cjs that don't have one

--- config/modules/vite.ts
+++ config/modules/vite.ts
@@ -1,3 +1,24 @@
-export default {
+import type { UserConfig } from "vite"
+
+const moduleConfig = {
   experimentWarning: false,
+}
+
+// Excluding the following modules from optimizing since they don't include a CJS file and already don't get optimized in the bundle.
+const viteConfig: UserConfig = {
+  build: {
+    commonjsOptions: {
+      exclude: [
+        "vue-notion",
+        "nuxt-webfontloader",
+        "@eggsydev/vue-lanyard",
+        "@nuxtjs/color-mode",
+      ],
+    },
+  },
+}
+
+export default {
+  ...moduleConfig,
+  ...viteConfig,
 }
