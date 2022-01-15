428c66f6dc443ed2c864e6a56f02c23e284801e1
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-15 18:04:11 +0100

    feat: create image container component for blog

---
+++ src/components/Blog/ImageContainer.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import Vue, { PropType } from "vue"
+
+export default Vue.extend({
+  props: {
+    type: {
+      type: String as PropType<"grid" | "flex">,
+      required: false,
+      default: "grid",
+    },
+  },
+})
+</script>
+
+<template>
+  <div
+    :class="{
+      'md:(flex space-x-2 items-center)': type === 'flex',
+      'grid md:grid-flow-col md:auto-cols-fr gap-2': type === 'grid',
+    }"
+  >
+    <slot />
+  </div>
+</template>
