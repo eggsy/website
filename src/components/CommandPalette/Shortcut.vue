8d128cad9bd2ae36d84ba0547bddab71afcde319
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-18 19:01:47 +0100

    feat: create shortcut component and show keye depending on system

---
+++ src/components/CommandPalette/Shortcut.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  data() {
+    return {
+      key: "⌘",
+    }
+  },
+  beforeMount() {
+    this.key = navigator?.platform?.includes("Mac") ? "⌘" : "Alt"
+  },
+})
+</script>
+
+<template>
+  <div
+    class="flex space-x-2 inset-x-0 bottom-8 items-center justify-center fixed"
+  >
+    <Kbd>{{ key }}</Kbd> <Kbd>K</Kbd>
+  </div>
+</template>
