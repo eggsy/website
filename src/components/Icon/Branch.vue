cb40ffcae5a1cd135170f5ed7f5b677f7c17060e
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-18 05:34:28 +0100

    chore: add new icons

---
+++ src/components/Icon/Branch.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path d="M6 3v12" />
+    <circle cx="18" cy="6" r="3" />
+    <circle cx="6" cy="18" r="3" />
+    <path d="M18 9a9 9 0 01-9 9" />
+  </svg>
+</template>
---
+++ src/components/Icon/Fork.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M7 12h7.5a3.5 3.5 0 003.47-3.04 3.5 3.5 0 111.006.008A4.5 4.5 0 0114.5 13H7v2.035a3.5 3.5 0 11-1 0v-6.07a3.5 3.5 0 111 0V12zm-3 6.5a2.5 2.5 0 105 0 2.5 2.5 0 00-5 0v0zm0-13a2.5 2.5 0 105 0 2.5 2.5 0 00-5 0v0zM18.5 3a2.5 2.5 0 100 5 2.5 2.5 0 000-5z"
+      fill="currentColor"
+      stroke="currentColor"
+    />
+  </svg>
+</template>
