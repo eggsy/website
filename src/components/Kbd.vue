22fe135e31b4e5734853e41682b1ed8f9b3d2ca6
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-18 05:34:36 +0100

    feat: Kbd component for keyboard keys

---
+++ src/components/Kbd.vue
@@ -0,0 +1,7 @@
+<template>
+  <kbd
+    class="rounded-lg cursor-pointer bg-neutral-400 py-2 px-4 transition-colors text-neutral-600 select-none dark:(bg-neutral-800 text-neutral-400 hover:bg-neutral-700) hover:bg-neutral-300"
+  >
+    <slot />
+  </kbd>
+</template>
