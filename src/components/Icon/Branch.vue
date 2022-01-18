0cf0981305029421f2897a9dd6b2bb3c75420e07
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-18 19:08:27 +0100

    style: better branch and fork icons

--- src/components/Icon/Branch.vue
+++ src/components/Icon/Branch.vue
@@ -1,16 +1,11 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path d="M6 3v12" />
-    <circle cx="18" cy="6" r="3" />
-    <circle cx="6" cy="18" r="3" />
-    <path d="M18 9a9 9 0 01-9 9" />
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M7 4.5v10M17 9.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM7 19.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM17 9.5A7.5 7.5 0 019.5 17"
+      stroke="currentColor"
+      stroke-width="1.667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
   </svg>
 </template>
