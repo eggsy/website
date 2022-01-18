f19caa89e79ec19901aca5be198c4b23f61725b2
Abdulbaki Dursun <eggsydev@gmail.com>
2022-01-18 19:10:58 +0100

    style: improve design of welcome component

--- src/components/CommandPalette/Items/Welcome.vue
+++ src/components/CommandPalette/Items/Welcome.vue
@@ -2,28 +2,30 @@
   <div class="space-y-2">
     <Title size="xs">Welcome to my website!</Title>
 
-    <CommandPaletteControls>
-      <Card class="px-4" :truncate="false" tight>
+    <CommandPaletteControls class="space-y-4">
+      <Card :truncate="false" tight>
         Welcome to Command Palette, a tool to help you out! You can use this
         window everywhere on the website! Don't forget the key combination!
       </Card>
 
-      <Card class="px-4" tight> Things you can do with this tool: </Card>
+      <Card tight> Things you can do with this tool: </Card>
 
       <div class="grid grid-cols-2 overflow-x-auto">
-        <Card :truncate="false">
+        <Card :truncate="false" tight>
           View latest blog posts that you can read
         </Card>
 
-        <Card :truncate="false">
+        <Card :truncate="false" tight>
           Quick access to some pages on the website
         </Card>
 
-        <Card :truncate="false"> View my recent activities on GitHub </Card>
-        <Card :truncate="false"> Some of my social media accounts </Card>
+        <Card :truncate="false" tight>
+          View my recent activities on GitHub
+        </Card>
+        <Card :truncate="false" tight> Some of my social media accounts </Card>
       </div>
 
-      <Card> Click on one of the items on the left to get started! </Card>
+      <Card tight> Click on one of the items on the left to get started! </Card>
     </CommandPaletteControls>
   </div>
 </template>
