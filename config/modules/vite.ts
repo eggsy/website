bac06234113092954a75664e0365d0bb62af9a7b
Abdulbaki Dursun <eggsydev@gmail.com>
2022-02-03 03:55:16 +0300

    chore: remove uneffective code

--- config/modules/vite.ts
+++ config/modules/vite.ts
@@ -1,26 +1,5 @@
-import type { UserConfig } from "vite"
-
-const moduleConfig = {
+export default {
   experimentWarning: false,
   build: false,
   ssr: false,
-}
-
-// Excluding the following modules from optimizing since they don't include a CJS file and already don't get optimized in the bundle.
-const viteConfig: UserConfig = {
-  build: {
-    commonjsOptions: {
-      exclude: [
-        "vue-notion",
-        "nuxt-webfontloader",
-        "@eggsydev/vue-lanyard",
-        "@nuxtjs/color-mode",
-      ],
-    },
-  },
-}
-
-export default {
-  ...moduleConfig,
-  ...viteConfig,
 }
