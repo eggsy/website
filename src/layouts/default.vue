a2194d151388e24f96b18c561cad58d8367f852b
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-14 01:54:17 +0300

    style: format file

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -45,7 +45,7 @@ export default Vue.extend({
 
     <!-- Nuxt component -->
     <Nuxt
-      class="container mx-auto min-h-screen pb-8 w-11/12 sm:(pb-10 w-9/12) md:w-7/12 "
+      class="container mx-auto min-h-screen pb-8 w-11/12 sm:(pb-10 w-9/12) md:w-7/12"
     />
 
     <!-- Footer -->
