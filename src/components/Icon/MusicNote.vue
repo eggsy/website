bcb6002c9bed51003c4f6b4a6e9e3295914a9862
Abdulbaki Dursun <eggsydev@gmail.com>
2022-07-14 01:54:08 +0300

    feat: add new icons

---
+++ src/components/Icon/MusicNote.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path
+      fill="currentColor"
+      d="M10 21q-1.65 0-2.825-1.175Q6 18.65 6 17q0-1.65 1.175-2.825Q8.35 13 10 13q.575 0 1.062.137q.488.138.938.413V3h6v4h-4v10q0 1.65-1.175 2.825Q11.65 21 10 21Z"
+    ></path>
+  </svg>
+</template>
