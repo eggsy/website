0b5f6b3c4ba1a655f85c65361061487f2a87d3dc
Abdulbaki Dursun <eggsydev@gmail.com>
2022-08-26 15:14:13 +0300

    refactor: add X button to close the modal

--- src/components/SponsorPopup.vue
+++ src/components/SponsorPopup.vue
@@ -21,25 +21,53 @@ export default Vue.extend({
 
 <template>
   <Transition name="fade" mode="out-in">
-    <SmartLink
+    <div
       v-if="viewed === false"
-      href="https://github.com/sponsors/eggsy"
-      blank
+      class="fixed inset-x-0 w-11/12 p-4 mx-auto space-y-1 rounded-lg shadow-md select-none bottom-4 lg:w-1/4 ring-1 ring-white/10 dark:ring-white/20 ring-black/20 lg:mx-0 lg:left-4 background-when-supports"
     >
-      <div
-        class="fixed inset-x-0 bottom-4 w-11/12 lg:w-1/4 backdrop-blur-md backdrop-filter p-4 ring-1 ring-white/10 mx-auto select-none shadow-md ring-1 dark:ring-white/20 ring-black/20 lg:mx-0 lg:left-4 dark:bg-black/50 rounded-lg space-y-2"
-        @click="dismissMessage"
-      >
-        <h3 class="text-lg font-medium dark:text-white leading-tight">
-          💖 Now you can sponsor me via GitHub Sponsors!
-        </h3>
+      <div class="flex items-center justify-between gap-2">
+        <SmartLink
+          href="https://github.com/sponsors/eggsy"
+          blank
+          @click.native="dismissMessage"
+        >
+          <h3 class="font-medium leading-tight dark:text-white">
+            💖 Sponsor me via GitHub Sponsors!
+          </h3>
+        </SmartLink>
 
-        <p class="dark:text-white/50 text-sm">
-          Sponsoring me and my work just got easier with GitHub Sponsors.
-          <span class="border-b dark:border-white/20">Click this box</span> to
-          see the tier list! Thank you for your support ✨
-        </p>
+        <button
+          class="p-1 transition-colors rounded-full hover:bg-black/20 dark:hover:bg-white/20 bg-black/10 dark:bg-white/10 dark:text-white"
+          @click="dismissMessage"
+        >
+          <IconX class="w-3 h-3" />
+        </button>
       </div>
-    </SmartLink>
+
+      <p class="text-sm light:opacity-50 dark:text-white/50">
+        Sponsoring me and my work just got easier with GitHub Sponsors.
+        <SmartLink
+          href="https://github.com/sponsors/eggsy"
+          blank
+          class="transition-colors border-b border-black/20 hover:border-black/40 dark:hover:border-white/40 dark:border-white/20"
+          @click.native="dismissMessage"
+        >
+          Click the title
+        </SmartLink>
+        to see the tier list! Thank you for your support ✨
+      </p>
+    </div>
   </Transition>
 </template>
+
+<style lang="scss" scoped>
+.background-when-supports {
+  @apply backdrop-blur-md backdrop-filter dark:bg-neutral-900;
+}
+
+@supports (backdrop-filter: blur()) {
+  .background-when-supports {
+    @apply backdrop-blur-md backdrop-filter dark:bg-neutral-800/10;
+  }
+}
+</style>
