5d7732bf42847e56c7c1b38eed4a2bd923df12f9
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-09 22:35:48 +0300

    refactor: less distractful callout style

--- src/components/Blog/Notification.vue
+++ src/components/Blog/Notification.vue
@@ -14,89 +14,46 @@ export default Vue.extend({
       default: "",
     },
   },
+  computed: {
+    getIcon() {
+      if (this.type === "warning") return "⚠️"
+      else if (this.type === "danger") return "🚨"
+      else if (this.type === "success") return "✅"
+      else return "💡"
+    },
+  },
 })
 </script>
 
 <template>
-  <div class="notification text-sm" :class="type">
-    <h1 v-if="title">{{ title }}</h1>
+  <div class="notification flex flex-col md:(items-center flex-row) gap-x-4 gap-y-2" :class="type">
+    <span class="text-xl md:text-lg">{{ getIcon }}</span>
 
-    <p v-if="!!$slots.default">
-      <slot />
-    </p>
+    <div>
+      <h1 v-if="title">{{ title }}</h1>
+
+      <p v-if="!!$slots.default">
+        <slot />
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .notification,
 .nuxt-content .notification {
-  @apply rounded-md space-y-2 border-l-4 p-4 dark:text-gray-100;
+  @apply rounded-lg border-[0.1px] my-5 p-4 bg-opacity-25 bg-neutral-300 border-neutral-200 dark:(bg-neutral-800/30 border-neutral-800);
 
   h1 {
-    @apply font-semibold text-base hover:no-underline;
+    @apply font-medium text-lg m-0 hover:no-underline;
   }
 
-  /* Background colors for types */
-  &.information {
-    @apply bg-blue-600 border-blue-500 text-blue-500;
-
-    h1 {
-      @apply text-blue-600;
-    }
-
-    p,
-    p strong {
-      @apply text-blue-500 dark:text-blue-400;
-    }
+  p,
+  p strong,
+  a {
+    @apply m-0 dark:text-white/70;
   }
 
-  &.warning {
-    @apply bg-yellow-500 border-yellow-500 text-yellow-500;
-
-    h1 {
-      @apply text-yellow-600;
-    }
-
-    p,
-    p strong {
-      @apply text-yellow-500 dark:text-yellow-400;
-    }
-  }
-
-  &.danger {
-    @apply bg-red-500 border-red-500 text-red-500;
-
-    h1 {
-      @apply text-red-600;
-    }
-
-    p,
-    p strong {
-      @apply text-red-500 dark:text-red-400;
-    }
-  }
-
-  &.success {
-    @apply bg-green-500 border-green-500 text-green-500;
-
-    h1 {
-      @apply text-green-600;
-    }
-
-    p,
-    p strong {
-      @apply text-green-500 dark:text-green-400;
-    }
-  }
-
-  &.information,
-  &.warning,
-  &.danger,
-  &.success {
-    @apply bg-opacity-25 dark:text-gray-100;
-  }
-
-  /* Other stlings */
   a {
     @apply font-medium text-current underline hover:underline;
   }
