a989460ab195c0429cefaa787501101e4f6ed29a
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-09 22:35:31 +0300

    feat: use html figure for better image captions

---
+++ src/components/Smart/Figure.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    alt: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    caption: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    imageClass: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+})
+</script>
+
+<template>
+  <figure class="w-full h-full overflow-hidden">
+    <component
+      :is="src.endsWith('.gif') ? 'img' : 'nuxt-img'"
+      :src="src"
+      :alt="alt || caption || 'image'"
+      loading="lazy"
+      draggable="false"
+      fit="cover"
+      class="object-cover w-full rounded-md"
+      :class="imageClass"
+    />
+
+    <figcaption v-if="caption" class="text-center">
+      {{ caption }}
+    </figcaption>
+  </figure>
+</template>
