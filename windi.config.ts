5d6f5be52a36f1ec921a8566a7b87a2d30156b27
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-10 01:47:38 +0300

    feat: add card style shortcut

--- windi.config.ts
+++ windi.config.ts
@@ -31,6 +31,8 @@ export default defineConfig({
   shortcuts: {
     "focus-ring":
       "ring-offset-2 light:(ring-offset-gray-50 ring-gray-300) focus:outline-none focus:ring-1 dark:focus:(ring-offset-neutral-900 ring-neutral-800)",
+    "card-base":
+      "border-[0.1px] p-4 bg-opacity-25 bg-neutral-100 hover:bg-neutral-200/40 border-neutral-200 dark:(bg-neutral-800/30 border-neutral-800 text-white/80 hover:text-white hover:bg-opacity-40) transition-colors ",
   },
   plugins: [
     lineClamp,
