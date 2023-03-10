25f5ee5ef3524c81341717936428c2f56e7885a6
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-10 15:17:45 +0300

    refactor: proper skeleton loader to match actual design

--- src/components/Loaders/Content.vue
+++ src/components/Loaders/Content.vue
@@ -15,32 +15,40 @@ export default Vue.extend({
 <template>
   <!-- Loader -->
   <div v-if="error === false" class="w-full pt-14">
-    <section class="space-y-4">
-      <!-- Reading time & date  -->
-      <div class="flex space-x-2 justify-center sm:justify-start">
-        <div
-          class="bg-gray-200 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-neutral-700"
-        />
-        <div
-          class="bg-gray-200 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-neutral-700"
-        />
-      </div>
-
-      <!-- Title -->
+    <section class="space-y-8">
+      <!-- Header -->
       <div
-        class="bg-gray-200 h-10 w-full animate-pulse sm:w-8/12 dark:bg-neutral-700"
+        class="bg-black/10 h-30 rounded-lg animate-pulse w-full dark:bg-white/5"
       />
 
-      <!-- Content -->
-      <div
-        class="flex flex-col space-y-2 items-center justify-center sm:items-start"
-      >
+      <div class="space-y-4">
+        <!-- Reading time & date  -->
+        <div class="flex space-x-2 justify-center">
+          <div
+            class="bg-black/10 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-white/5"
+          />
+          <div
+            class="bg-black/10 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-white/5"
+          />
+          <div
+            class="bg-black/10 h-4 animate-pulse w-1/4 sm:w-1/6 dark:bg-white/5"
+          />
+        </div>
+
+        <!-- Title -->
         <div
-          class="bg-gray-200 h-4 w-full animate-pulse sm:w-8/12 dark:bg-neutral-700"
+          class="bg-black/10 h-10 mx-auto w-full animate-pulse sm:w-8/12 dark:bg-white/5"
         />
-        <div
-          class="bg-gray-200 h-4 animate-pulse w-1/2 sm:w-4/12 dark:bg-neutral-700"
-        />
+
+        <!-- Content -->
+        <div class="flex flex-col space-y-2 items-center justify-center">
+          <div
+            class="bg-black/10 h-4 w-full animate-pulse sm:w-8/12 dark:bg-white/5"
+          />
+          <div
+            class="bg-black/10 h-4 animate-pulse w-1/2 sm:w-4/12 dark:bg-white/5"
+          />
+        </div>
       </div>
     </section>
 
@@ -50,7 +58,7 @@ export default Vue.extend({
         <div
           v-for="_ in 6"
           :key="_"
-          class="bg-gray-200 h-4 animate-pulse dark:bg-neutral-700"
+          class="bg-black/10 h-4 animate-pulse dark:bg-white/5"
           :class="_ !== 6 ? 'w-full' : 'w-8/12'"
         />
       </div>
@@ -59,7 +67,7 @@ export default Vue.extend({
         <div
           v-for="_ in 4"
           :key="_"
-          class="bg-gray-200 h-4 animate-pulse dark:bg-neutral-700"
+          class="bg-black/10 h-4 animate-pulse dark:bg-white/5"
           :class="_ !== 4 ? 'w-full' : 'w-1/2'"
         />
       </div>
@@ -68,7 +76,7 @@ export default Vue.extend({
         <div
           v-for="_ in 24"
           :key="_"
-          class="bg-gray-200 h-4 animate-pulse dark:bg-neutral-700"
+          class="bg-black/10 h-4 animate-pulse dark:bg-white/5"
           :class="_ !== 24 ? 'w-full' : 'w-1/2'"
         />
       </div>
