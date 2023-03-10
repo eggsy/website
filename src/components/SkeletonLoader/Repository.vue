92eff9bd7f9f031c29af990cf6aa87e359bff99e
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-10 15:26:04 +0300

    style: proper designed skeleton loader

--- src/components/SkeletonLoader/Repository.vue
+++ src/components/SkeletonLoader/Repository.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="rounded-md space-y-4 p-4">
+  <div class="rounded-md space-y-4 card-base">
     <div
-      class="rounded-md bg-gray-300 h-5 animate-pulse w-7/12 dark:bg-neutral-700/40"
+      class="rounded-md bg-black/10 h-6 animate-pulse w-7/12 dark:bg-white/5"
     />
 
     <div class="space-y-2">
       <div
-        class="rounded-md bg-gray-300 h-4 w-full animate-pulse dark:bg-neutral-700/40"
+        class="rounded-md bg-black/10 h-4 w-full animate-pulse dark:bg-white/5"
       />
       <div
-        class="rounded-md bg-gray-300 h-4 animate-pulse w-4/12 dark:bg-neutral-700/40"
+        class="rounded-md bg-black/10 h-4 animate-pulse w-4/12 dark:bg-white/5"
       />
     </div>
 
     <div class="space-y-2">
       <div class="flex items-center justify-between">
         <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+          class="rounded-md bg-black/10 h-4 animate-pulse w-1/6 dark:bg-white/5"
         />
         <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
-        />
-      </div>
-
-      <div class="flex items-center justify-between">
-        <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-2/6 dark:bg-neutral-700/40"
-        />
-        <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+          class="rounded-md bg-black/10 h-4 animate-pulse w-1/6 dark:bg-white/5"
         />
       </div>
 
       <div class="flex items-center justify-between">
         <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+          class="rounded-md bg-black/10 h-4 animate-pulse w-2/6 dark:bg-white/5"
         />
         <div
-          class="rounded-md bg-gray-300 h-4 animate-pulse w-1/6 dark:bg-neutral-700/40"
+          class="rounded-md bg-black/10 h-4 animate-pulse w-1/6 dark:bg-white/5"
+        />
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div
+          class="rounded-md bg-black/10 h-4 animate-pulse w-1/6 dark:bg-white/5"
+        />
+        <div
+          class="rounded-md bg-black/10 h-4 animate-pulse w-1/6 dark:bg-white/5"
         />
       </div>
     </div>
