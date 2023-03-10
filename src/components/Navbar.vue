84c41a969183956cf11dfb823b5e52629b8498eb
Abdulbaki Dursun <eggsydev@gmail.com>
2023-03-10 17:49:47 +0300

    feat: display blog next to logo on blog pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,15 +3,15 @@ import Vue from "vue"
 
 export default Vue.extend({
   computed: {
-    /**
-     * Return target url.
-     */
-    getTargetLink(): string {
+    routeIsBlog() {
+      return this.$route.name?.includes("blog")
+    },
+    getTargetLink() {
       const path = this.$route.path
 
       if (path !== "/projects/premid" && path.startsWith("/projects/premid"))
         return "/projects/premid"
-      else if (path.includes("/blog/") && this.$route.params) return "/blog"
+      else if (this.routeIsBlog && this.$route.params?.slug) return "/blog"
       else return "/"
     },
   },
@@ -21,15 +21,24 @@ export default Vue.extend({
 <template>
   <nav class="w-full">
     <div class="container mx-auto w-11/12 relative sm:w-9/12 md:w-7/12">
-      <div class="flex items-center justify-between">
-        <SmartLink :href="getTargetLink" class="flex-shrink-0 h-10 w-10">
+      <div class="flex items-center gap-6 justify-between">
+        <SmartLink
+          :href="getTargetLink"
+          class="flex-shrink-0 h-10 w-10 flex items-center space-x-4"
+        >
           <SmartImage
             src="/assets/icons/icon.svg"
-            class="rounded-md bg-gray-100 ring-black transition-transform ring-2 ring-opacity-5 sm:transform hover:-rotate-6"
-            width="100"
-            height="100"
-            title="EGGSY's Website"
+            class="rounded-md bg-gray-100 ring-black transition-transform ring-2 ring-opacity-5 sm:transform hover:-rotate-6 flex-shrink-0 h-10 w-10"
           />
+
+          <Transition name="fade" mode="out-in">
+            <span
+              v-if="routeIsBlog"
+              class="text-black/90 text-sm uppercase font-medium dark:text-white/90"
+            >
+              Blog
+            </span>
+          </Transition>
         </SmartLink>
 
         <div class="flex space-x-2 items-center">
